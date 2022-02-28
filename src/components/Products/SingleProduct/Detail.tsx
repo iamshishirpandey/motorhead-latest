6d5e2bb3ec063dd29616e6fb253e4601330044b7
nikhilpantha <nikhilpantha207@gmail.com>
2022-02-28 20:56:17 +0545

    photo of tekken bike

--- src/components/Products/SingleProduct/Detail.tsx
+++ src/components/Products/SingleProduct/Detail.tsx
@@ -90,7 +90,7 @@ const Details = (props: Props) => {
   ];
 
   return (
-    <div className=" border border-gary-200 lg:border-none lg:p-10 xl:px-28">
+    <div className=" border border-gary-200 lg:border-none lg:px-10 pb-10 xl:px-28">
       <div className="bg-white grid sm:grid-cols-4 gap-5 sm::gap-10 rounded overflow-hidden">
         <ul className="w-full border-r-2">
           {tabs.map((item, index) => (
@@ -104,11 +104,11 @@ const Details = (props: Props) => {
             </li>
           ))}
         </ul>
-        <div className="p-5 sm:p-10 sm:col-span-3 space-y-10">
+        <div className="p-5 sm:col-span-3 space-y-10">
           {tabs.map((item, index) => (
             <div key={index}>
               {item.id === show && (
-                <div key={index} className=" space-y-5 sm:space-y-10">
+                <div key={index} className="space-y-5 sm:space-y-10">
                   <h1 className="px-5 border-l-2 border-red-700 text-2xl font-medium">
                     {item.name}
                   </h1>
--- src/components/Products/SingleProduct/ProductDetails.tsx
+++ src/components/Products/SingleProduct/ProductDetails.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const ProductDetails = (props: Props) => {
   return (
-    <div className="bg-gray-100 lg:p-10 xl:p-28">
+    <div className="bg-gray-100 lg:p-10 xl:px-28 xl:py-16 ">
       <div className="bg-white p-5 lg:p-10 rounded grid grid-cols-1 md:grid-cols-2 items-center gap-10">
         <div>
           <Swipers />
