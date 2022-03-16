8ee33fcf7f5fbe812a92f1ad571e4f54295f511e
nikhilpantha <nikhilpantha207@gmail.com>
2022-03-16 15:05:28 +0545

    links

--- src/components/Home/Blogs.tsx
+++ src/components/Home/Blogs.tsx
@@ -15,15 +15,15 @@ const Blogs = (props: Props) => {
   const [ind, setInd] = useState(0);
 
   return (
-    <div className="pb-10 sm:pb-20 px-5 lg:px-10  xl:px-28">
-      <div className="text-center text-4xl font-bold py-16 uppercase">
+    <div className="pb-10 sm:pb-20 px-5  lg:px-10  xl:px-28">
+      <div className="sm:text-center text-4xl font-bold py-16 uppercase">
         News & Events
       </div>
       {/* <div className="text-center w-full flex flex-col justify-center items-center space-y-5">
         <BlogCard />
       </div> */}
       <div className="md:col-span-2 sm:p-5 lg:p-10 bg-white rounded space-y-5 lg:space-y-10">
-        <div className="p-5 sm:p-0 space-y-2">
+        <div className="space-y-2">
           <h1 className=" text-2xl text-red-700">
             Authorized Service Provider
           </h1>
@@ -31,14 +31,14 @@ const Blogs = (props: Props) => {
             Authorized Service Provider Opening in Chahabil on Poush 15, 2078.
           </p>
         </div>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 ">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-0">
           {images.map((item, index) => (
             <img
               key={index}
               src={item}
               alt={item}
               className={`${
-                show && ind === index ? "p-0" : "p-5"
+                show && ind === index ? "sm:p-0" : "sm:p-5"
               }   w-full h-auto ease-in-out duration-300 bg-cover top-0`}
               onMouseOver={() => {
                 setShow(true);
