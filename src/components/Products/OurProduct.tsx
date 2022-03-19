bec01e4253d8d6611dbef41e9a516ab77aec65f1
nikhilpantha <nikhilpantha207@gmail.com>
2022-03-19 09:21:05 +0545

    search function

--- src/components/Products/OurProduct.tsx
+++ src/components/Products/OurProduct.tsx
@@ -20,11 +20,19 @@ const OurProduct = (props: Props) => {
   return (
     <div className="grid sm:grid-cols-3 gap-5 lg:gap-10 bg-gray-100 p-5 lg:p-10 xl:p-28">
       <div className="sm:col-span-2 grid md:grid-cols-2 gap-5">
-        {filteredProduct.map((item, index) => (
-          <div key={index}>
-            <ProductCard bike={item} />
+        {filteredProduct.length === 0 ? (
+          <div className="md:col-span-2 text-center font-medium text-xl">
+            Sorry, No item Found !!
           </div>
-        ))}
+        ) : (
+          <>
+            {filteredProduct.map((item, index) => (
+              <div key={index}>
+                <ProductCard bike={item} />
+              </div>
+            ))}
+          </>
+        )}
       </div>
       <div className="hidden sm:block bg-white">
         <SideBar setFilter={setFilter} />
